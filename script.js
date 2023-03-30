a83ea7745175d8f2d6219c84613e32a7c1277e4f
Abdisalam <abdisalamfresh@gmail.com>
2023-03-29 22:12:41 -0700

    not many changes added a modal and changed the modal a little will modified later

--- script.js
+++ script.js
@@ -8,21 +8,46 @@ const showModalTotal = document.getElementById('showModalTotal');
 
 
 
+// initialize variables
+let headCount = 0;
+
+
 // Add Event Listener 
 calculateBtn.addEventListener('click', handleClick);
-costInput.addEventListener('focus', (e) => {
-    const currentValue = parseFloat(e.target.value);
-    e.target.value = currentValue.toLocaleString('en-US', {style: 'currency', currency: 'USD'});
-})
+decreamentCountBtn.addEventListener('click', () => {
+    headCount--;
+    personToAdd.textContent = headCount;
+});
+increaseCountBtn.addEventListener('click', () => {
+    headCount++;
+    personToAdd.textContent = headCount;
+});
 
 
-function handleClick(){
-    //grab the users amount that was inserted
-    const inputedCost = costInput.value;
-    const inputedTip = tipInput.value;
 
+
+
+function handleClick(e){
+    e.preventDefault();
+    
+    calculateTotal(inputedCost, inputedTip);
+    
+    console.log(`Cost: ${inputedCost}`);
+    console.log(`Tip: ${inputedTip}%`);
+    console.log(`Tip amount: ${tipAmount.toFixed(2)}`);
+    console.log(`Total: ${total.toFixed(2)}`)
    
+}
+
+
+function calculateTotal(cost, tip){
     
-    
-    console.log(inputedCost + inputedTip)
+    //grab the users amount that was inserted
+    const cost = parseFloat(costInput.value);
+    const tip = parseFloat(tipInput.value);
+
+
+    const tipAmount = cost * (tip / 100)
+    const total = cost + tip;
+
 }
